8073746a64dfae5e62e9a10e065ab987352cf558
kbtutorials <gauthamkarthik534@gmail.com>
2023-10-02 17:52:51 +0530

    Random Jokes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState } from "react";
+import "./App.css";
+import { typeImplementation } from "@testing-library/user-event/dist/type/typeImplementation";
 
 function App() {
+  const jokesObj = {
+    type: "general",
+    setup: "What did the spaghetti say to the other spaghetti?",
+    punchline: "Pasta la vista, baby!",
+    id: 150,
+  };
+  const [jokes, setJokes] = useState(jokesObj);
+
+  async function getJoke() {
+    const response = await fetch(
+      "https://official-joke-api.appspot.com/random_joke"
+    );
+    const data = await response.json();
+    setJokes(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={getJoke}>Random Joke</button>
+      </section>
+      <section>
+        <h2>
+          <i>Setup : </i>
+          {jokes.setup}
+        </h2>
+        <h2>
+          <i>Punchline: </i>
+          {jokes.punchline}
+        </h2>
+      </section>
+    </React.Fragment>
   );
 }
 
